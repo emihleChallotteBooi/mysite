cefc5a4ae3c80caa7bc1e208e7ccd5c7778378d7
Emihle Booi <2671438@students.wits.ac.za>
2025-06-26 12:52:03 +0200

    worked on the layout of all the pages.i did the routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Home from './pages/landing.js'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Landing from './pages/landing';
+import Home from './pages/home';
+import Software from './pages/software';
+import Modelling from './pages/modelling';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/software" element={<Software />} />
+        <Route path="/modelling" element={<Modelling />} />
+      </Routes>
+    </Router>
   );
 }
 
