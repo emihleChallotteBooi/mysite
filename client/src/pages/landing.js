75d4936ffe08a2dd62ea40878db42375797baf9c
emihleChallotteBooi <bemihle9@gmail.com>
2025-06-26 09:14:06 +0200

    the content for landing page, no structure or layout order has been enforced yet. just the material

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,11 +1,35 @@
 import React from "react";
+import Footer from "../components/footer";
 
 function Landing() {
   return (
-    <div>
-      <h1>Welcome to My Personal Website</h1>
-      <p>This is the landing page.</p>
-    </div>
+     <>
+     <header></header><main>
+      <section id="left">
+    <section id="heading">
+      <h1>Welcome to</h1>
+      <h1>Q_Challotte </h1>
+
+    </section>
+       <section id="paragraph">
+     <p> Hey there, I'm a developer, model and the most optimistic person you'll ever meet.
+      Inside you will find my work , software projects and interests.
+     </p>
+ <p>
+  This platform aims to reach audience that will appreciate my work and hopefully be inclined to feature me in opportunities they might have.Please reach out to me using the social icons.  
+ </p>
+    </section>
+
+      </section>
+
+      <section id="image">
+
+      </section>
+
+
+    </main>
+    <Footer />
+    </>
   );
 }
 
